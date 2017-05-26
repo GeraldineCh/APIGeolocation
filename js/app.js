24ea1e9a61ff0ec5009fea6db908d95b0e0c6413
Geraldine Chauca <gchaucae@gmail.com>
2017-05-26 11:58:26 -0500

    Clase 26 de mayo - Trazado ruta

--- js/app.js
+++ js/app.js
@@ -38,5 +38,37 @@ function initMap() {
 	
 
 	document.getElementById("encuentrame").addEventListener("click",buscar);
-	
+
+	/*Día 26 de mayo*/
+	var inputPartida = document.getElementById("punto-partida");
+	var inputDestino = document.getElementById("punto-destino");
+
+	new google.maps.places.Autocomplete(inputPartida);	
+	new google.maps.places.Autocomplete(inputDestino);
+
+	var directionsService = new google.maps.DirectionsService;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
+
+	var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
+	    directionsService.route({
+	      origin: inputPartida.value,
+	      destination: inputDestino.value,
+	      travelMode: 'DRIVING'
+	    }, function(response, status){
+	        if (status === 'OK') {
+	          directionsDisplay.setDirections(response);
+	        }else{
+	          alert("No encontramos una ruta.");
+	        }
+	    });
+	}
+
+	directionsDisplay.setMap(map);
+
+	var trazarRuta = function(e){
+		e.preventDefault(); //Por tener validaciones en el form
+    	calculateAndDisplayRoute(directionsService, directionsDisplay);
+  	}  
+	document.getElementById("trazar-ruta").addEventListener("click",trazarRuta);    
+
 }
